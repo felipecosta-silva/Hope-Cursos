47d1654a06af67f316312ae75f8f245dd3d41c9b
Felipe Costa <felipecostati@gmail.com>
2020-02-18 01:21:49 -0300

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -3,15 +3,9 @@ include_once("cabecalho.php")
 ?>
 
 <body>
-
-</body>
-
 <section class="bg-light page-section" id="cursosPage">
   <div class="container">
-
-
     <div class="row">
-
       <div class="col-md-3 col-sm-6 cursos-item">
         <a class="cursos-link" href="#">
           <div class="cursos-hover">
@@ -31,7 +25,6 @@ include_once("cabecalho.php")
           </a>
         </div>
       </div>
-
       <div class="col-md-3 col-sm-6 cursos-item">
         <a class="cursos-link" href="#">
           <div class="cursos-hover">
@@ -49,8 +42,6 @@ include_once("cabecalho.php")
           </a>
         </div>
       </div>
-
-
       <div class="col-md-3 col-sm-6 cursos-item">
         <a class="cursos-link" href="#">
           <div class="cursos-hover">
@@ -68,8 +59,6 @@ include_once("cabecalho.php")
           </a>
         </div>
       </div>
-
-
       <div class="col-md-3 col-sm-6 cursos-item">
         <a class="cursos-link" href="#">
           <div class="cursos-hover">
@@ -87,12 +76,76 @@ include_once("cabecalho.php")
           </a>
         </div>
       </div>
+    <div class="container">
+     <div class="row">
+
+        <div class="col-md-3 col-sm-6 cursos-item">
+          <a class="cursos-link" href="#">
+            <div class="cursos-hover">
+              <div class="cursos-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/curso.jpg" alt="">
+          </a>
+          <div class="cursos-caption">
+            <h5>Front-End WEB</h5>
+            <p class="text-muted">Desenvolvimento de Sites</p>
+             <a href="#" class="text-muted"><p class="valor_curso">R$ 55,00</p></a>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 cursos-item">
+          <a class="cursos-link" href="#">
+            <div class="cursos-hover">
+              <div class="cursos-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/curso.jpg" alt="">
+          </a>
+          <div class="cursos-caption">
+            <h5>Front-End WEB</h5>
+            <p class="text-muted">Desenvolvimento de Sites</p>
+             <a href="#" class="text-muted"><p class="valor_curso">R$ 55,00</p></a>
+          </div>
+        </div>
 
 
+        <div class="col-md-3 col-sm-6 cursos-item">
+          <a class="cursos-link" href="#">
+            <div class="cursos-hover">
+              <div class="cursos-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/curso.jpg" alt="">
+          </a>
+          <div class="cursos-caption">
+            <h5>Front-End WEB</h5>
+            <p class="text-muted">Desenvolvimento de Sites</p>
+             <a href="#" class="text-muted"><p class="valor_curso">R$ 55,00</p></a>
+          </div>
+        </div>
 
 
+        <div class="col-md-3 col-sm-6 cursos-item">
+          <a class="cursos-link" href="#">
+            <div class="cursos-hover">
+              <div class="cursos-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/curso.jpg" alt="">
+          </a>
+          <div class="cursos-caption">
+            <h5>Front-End WEB</h5>
+            <p class="text-muted">Desenvolvimento de Sites</p>
+             <a href="#" class="text-muted"><p class="valor_curso">R$ 55,00</p></a>
+          </div>
+        </div>
     </div>
   </div>
+  </body>
 </section>
 
-</html>
