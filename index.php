c169b3741c05e048274fc57817a790cb4de494c0
Felipe Costa <felipecostati@gmail.com>
2020-03-19 00:58:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,12 @@ include_once("cabecalho.php")
 ?>
 
 
-
 <!-- CABEÇALHO -->
 <header class="cabecalho">
   <div class="container">
     <div class="intro-text">
       <div class="intro-lead-in">
         Bem Vindo a Q-Cursos
-      </div>
-      <div class="intro-heading text-uppercase">
-        Ensino a Distância
-      </div>
       </div>
       <div class="intro-heading text-uppercase">
         Ensino a Distância
@@ -30,19 +25,6 @@ include_once("cabecalho.php")
       <div class="col-lg-12 text-center">
         <h2 class="section-heading text-uppercase">Principais Formações</h2>
         <h3 class="section-subheading text-muted"><a class="text-muted" href="#">Ver todas as demais Formações</a></h3>
-  <!-- CABEÇALHO -->
-
-  <header class="cabecalho">
-    <div class="container">
-      <div class="intro-text">
-        <div class="intro-lead-in">
-          Bem Vindo a Q-Cursos
-        </div>
-        <div class="intro-heading text-uppercase">
-          Ensino a Distância
-        </div>
-        <a class="btn btn-primary btn-xl text-uppercase js-scrool-trigger" href="#cursos">Nossos Cursos</a>
-
       </div>
     </div>
     <div class="row text-center">
@@ -441,10 +423,10 @@ include_once("cabecalho.php")
       <div class="col-md-4">
         <ul class="list-inline quicklinks">
           <li class="list-inline-item">
-            <a href="#">Hugo Vasconcelos</a>
+            <a href="#">Felipe Costa</a>
           </li>
           <li class="list-inline-item">
-            <a class="text-muted" href="http://api.whatsapp.com/send?1=pt_BR&phone=5581999999999" alt="81 99999-9999" target="_blank"><i class="fab fa-whatsapp mr-1"></i>81 99999-9999</a>
+            <a class="text-muted" href="http://api.whatsapp.com/send?1=pt_BR&phone=5581999999999" alt="81 99999-9999" target="_blank"><i class="fab fa-whatsapp mr-1"></i>81 9523-6005</a>
           </li>
         </ul>
       </div>
@@ -455,8 +437,3 @@ include_once("cabecalho.php")
 </body>
 
 </html>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
